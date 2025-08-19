24d6f1e0f6bfb95369dc8e9dda862667b1ff9785
Alina Zubko <ali1304sha@gmail.com>
2025-08-19 23:50:36 +0300

    code edits

--- service/post_service.go
+++ service/post_service.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"strings"
 	"time"
+	"unicode/utf8"
 
 	"github.com/AlinaZbk/mini-blog.git/model"
 )
@@ -16,7 +17,7 @@ var (
 func CreatePost(req model.CreatePostRequest) (*model.Post, error) {
 	title := strings.TrimSpace(req.Title)
 	content := strings.TrimSpace(req.Content)
-	if title == "" || len(title) > 200 {
+	if title == "" || utf8.RuneCountInString(title) > 200 {
 		return nil, errors.New("title is required and must be <= 200 chars")
 	}
 	if content == "" {
@@ -47,8 +48,8 @@ func GetPost(id int64) (*model.Post, error) {
 }
 
 func DeletePost(id int64) error {
-	for i, p := range posts {
-		if p.ID == id {
+	for i, post := range posts {
+		if post.ID == id {
 			posts = append(posts[:i], posts[i+1:]...)
 			return nil
 		}
@@ -59,19 +60,19 @@ func DeletePost(id int64) error {
 func UpdatePost(id int64, req model.UpdatePostRequest) (*model.Post, error) {
 	title := strings.TrimSpace(req.Title)
 	content := strings.TrimSpace(req.Content)
-	if title == "" || len(title) > 200 {
+	if title == "" || utf8.RuneCountInString(title) > 200 {
 		return nil, errors.New("title is required and must be <= 200 chars")
 	}
 	if content == "" {
 		return nil, errors.New("content is required")
 	}
 
-	for i, p := range posts {
-		if p.ID == id {
-			posts[i].Title = req.Title
-			posts[i].Content = req.Content
-			posts[i].UpdatedAt = time.Now()
-			return posts[i], nil
+	for _, post := range posts {
+		if post.ID == id {
+			post.Title = req.Title
+			post.Content = req.Content
+			post.UpdatedAt = time.Now()
+			return post, nil
 		}
 	}
 	return nil, errors.New("post not found")
